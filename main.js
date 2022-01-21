b58379f1ac58ddaaf9bc40fc5d4423f5aacecf88
SatouKz <alexander.ajalla404@gmail.com>
2022-01-21 17:23:13 -0300

    avance-3

--- main.js
+++ main.js
@@ -5,15 +5,13 @@ const container = document.getElementById("container");
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
-
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
-
 const signUpxButton = document.getElementById("signUpx");
 const signInxButton = document.getElementById("signInx");
-
-signUpxButton.addEventListener("click", function capturar() {
+// Evento que recopila los datos del usuario
+signUpxButton.addEventListener("click", function ssRegister() {
   function usuario(name, email, password) {
     this.name = name;
     this.email = email;
@@ -23,19 +21,33 @@ signUpxButton.addEventListener("click", function capturar() {
   let emailCapture = document.getElementById("email").value;
   let passCapture = document.getElementById("password").value;
   // console.log(nameCapture + " " + emailCapture + " " + passCapture);
+  // Creación de objeto en base a los datos del usuario
   const nuevoUsuario = new usuario(nameCapture, emailCapture, passCapture);
-  console.log(nuevoUsuario);
-  console.log(JSON.stringify(nuevoUsuario));
-
-fetch("https://tiendavirtualmern.herokuapp.com/api",{
-  
-})
+  // console.log(nuevoUsuario);
+  // console.log(JSON.stringify(nuevoUsuario));
+  // REGISTER
   fetch("https://tiendavirtualmern.herokuapp.com/api/users", {
     method: "POST",
     body: JSON.stringify(nuevoUsuario),
     headers: { "Content-Type": "application/json" },
   })
-    .then(response => response.json())
-    .then(data => console.log(data))
-})
-.catch(err => console.error(err))
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
+//LOGIN
+signInxButton.addEventListener("click", function ssLogin() {
+  function user(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+  let emailCapture = document.getElementById("email").value;
+  let passCapture = document.getElementById("password").value;
+  const userLogin = new user(emailCapture, passCapture);
+  fetch("https://tiendavirtualmern.herokuapp.com/api/users/login", {
+    method: "POST",
+    body: JSON.stringify(userLogin),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
