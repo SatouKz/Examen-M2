ead824ae5986efdeb58d66ae3b3d250c3c5aace4
SatouKz <alexander.ajalla404@gmail.com>
2022-01-20 21:29:30 -0300

    avance

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-  container.classList.add('right-panel-active');
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-  container.classList.remove('right-panel-active');
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
 
-// fetch('https://tiendavirtualmern.herokuapp.com/api',{
-// })
-signUpButton.addEventListener('click', () => {
-  const nuevoUser = {
-    name: '',
-    email: '',
-    password: '',
-  };
-  console.log(nuevoUser);
-  // fetch ('https://tiendavirtualmern.herokuapp.com/api/users',{
-  //   method: 'POST',
-  //   body: JSON.stringify(nuevoUser),
-  //   headers: {}
-  // })
-})
+const signUpxButton = document.getElementById("signUpx");
+const signInxButton = document.getElementById("signInx");
+
+function capturar() {
+  function usuario(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  let nameCapture = document.getElementById("name").value;
+  let emailCapture = document.getElementById("email").value;
+  let passCapture = document.getElementById("password").value;
+  // console.log(nameCapture + " " + emailCapture + " " + passCapture);
+  const nuevoUsuario = new usuario(nameCapture, emailCapture, passCapture);
+  console.log(nuevoUsuario);
+}
